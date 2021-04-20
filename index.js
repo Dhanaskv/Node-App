b726e4f5d2838bf39858f1cc1f84801e2d34da58
Dhanasekar K <70059744+Dhanaskv@users.noreply.github.com>
2021-04-20 18:24:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World! 1234');
+  res.end('Hello World!  TrueHoch');
 }).listen(8080);
